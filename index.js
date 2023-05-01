8e925885cc17e114555b170ddbc27d349248b7f3
Daniel-Williams-ux <williamsoligodaniel@gmail.com>
2023-05-01 05:37:01 +0000

    Create a new connection to the chat server

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 //Add the Socket client and readline modules
 const io = require("socket.io-client");
 const readline = require("readline");
+
+//Create a new connection to the chat server
+var socket = io("https://repl-chat-server.DanielWilliams23.repl.co");
