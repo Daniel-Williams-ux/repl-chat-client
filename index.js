42848176ff1d6ed2b6f2163a31f7a190f8d0a883
Daniel-Williams-ux <williamsoligodaniel@gmail.com>
2023-05-01 05:45:13 +0000

    Getting the user chat handle

--- index.js
+++ index.js
@@ -20,3 +20,16 @@ socket.on("connect", function () {
   get_chat_handle();
   socket.on("broadcast", display_message);
 });//this code calls two methods: get_chat_handle and display_message
+
+
+// Gets the user's name, so we can introduce and prepend each message with their name
+function get_chat_handle() {
+  chat_interface.question(
+    `Hello! What's your chat handle? `,
+    function (answer) {
+      chat_handle = answer;
+      socket.emit("message", chat_handle + " has entered the chat");
+      chat();
+    }
+  );
+}
